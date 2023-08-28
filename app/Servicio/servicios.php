29bafeb84ae43cd840d54efa95a9f72e73b5c916
KevinNavarr <102337594+KevinNavarr@users.noreply.github.com>
2023-08-27 19:47:19 -0500

    Create servicios.php

---
+++ app/Servicio/servicios.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Servicio;
+
+class serviciosServicio
+{
+    
+        public function Create($data)
+        {
+            //
+        }
+
+        public function Update($data)
+        {
+            //
+        }
+
+        public function Delete($data)
+        {
+            //
+        }
+
+        public function Consultar($data)
+        {
+            //
+        }
+
+
+        
+}
