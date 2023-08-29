27abe0a9ee1d5cb780abb4e51e553396c24bbbe9
KevinNavarr <102337594+KevinNavarr@users.noreply.github.com>
2023-08-29 15:51:44 -0500

    CRUD
    CRUD de carrera e instituto

---
+++ app/Servicio/CarreraServicio.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Servicio;
+
+use App\Http\Responses\TypeResponse;
+use App\Models\CarreraModel;
+use Exception;
+
+class CarreraServicio
+{
+    protected $obj_carrera_modelo;
+    protected $obj_tipo_respuesta;
+    public function __construct()
+    {
+        $this->obj_carrera_modelo = new CarreraModel();
+        $this->obj_tipo_respuesta = new TypeResponse();
+    }
+    
+        public function CreateCarrera($carreraData)
+        {
+            try {
+                //crear nueva carrera
+                $nuevaCarrera = new CarreraModel();
+                $nuevaCarrera->nombre = $carreraData['nombre'];
+                $nuevaCarrera->codigo = $carreraData['codigo'];
+
+                $nuevaCarrera->save();
+
+                $this->obj_tipo_respuesta->setok(true);
+                $this->obj_tipo_respuesta->setdata($nuevaCarrera);
+
+            }catch (Exception $e) {
+                $this->obj_tipo_respuesta->setok(false);
+                $this->obj_tipo_respuesta->seterror('Error al crear la carrera', false);
+            }
+            return $this->obj_tipo_respuesta->getdata();
+        }
+
+        public function UpdateCarrera($carreraData)
+        {
+            try {
+                //se busca la carrera a editar utilizando el modelo CarreraModel
+                $carrera = CarreraModel::findOrFail($carreraData['id_carrera']);
+
+                $carrera->nombre = $carreraData['nombre'];
+                $carrera->codigo = $carreraData['codigo'];
+
+                $carrera->save();
+
+                $this->obj_tipo_respuesta->setok(true);
+                $this->obj_tipo_respuesta->setdata($carrera);
+            }catch (Exception $e) {
+                $this->obj_tipo_respuesta->setok(false);
+                $this->obj_tipo_respuesta->seterror('Error al editar la carrera', false);
+            }
+            return $this->obj_tipo_respuesta->getdata();
+        }
+
+        public function Delete($carreraData)
+        {
+            try {
+                //se busca la carrera a eliminar
+                $carrera = CarreraModel::findOrFail($carreraData);
+                
+                $carrera->delete();
+                
+                $this->obj_tipo_respuesta->setok(true);
+                $this->obj_tipo_respuesta->setdata(null);// no hay datos para devolver después de eliminar
+            }catch (Exception $e) {
+                $this->obj_tipo_respuesta->setok(false);
+                $this->obj_tipo_respuesta->seterror('Error al eliminar la carrera', false);
+            }
+            return $this->obj_tipo_respuesta->getdata();
+        }
+
+        public function Consultar($data)
+        {
+            $datos = null;
+            try {
+                switch($data["tipo_consulta"]){
+                    case 1:
+                        //consula por código
+                        $datos = CarreraModel::where('codigo', $data["data"])->get();
+                    case 2:
+                        $datos = CarreraModel::where('nombre', 'LIKE', '%' . $data["data"] . '%')->get();
+                }
+                $this->obj_tipo_respuesta->setdata($datos[0]);
+
+            }catch (Exception $e) {
+                $this-> obj_tipo_respuesta->setok(false);
+                $this->obj_tipo_respuesta->seterror('Lo sentimos error en la consulta', false);
+            }
+            return $this->obj_tipo_respuesta->getdata();
+        }
+
+
+        
+}
---
+++ app/Servicio/InstitutoServicio.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Servicio;
+
+use App\Http\Responses\TypeResponse;
+use App\Models\InstitutoModel;
+use Exception;
+
+class InstitutoServicio
+{
+    protected $obj_instituto_modelo;
+    protected $obj_tipo_respuesta;
+    public function __construct()
+    {
+        $this->obj_instituto_modelo = new InstitutoModel();
+        $this->obj_tipo_respuesta = new TypeResponse();
+    }
+    
+        public function CreateInstituto($institutoData)
+        {
+            try {
+                //crear nuevo instituto
+                $nuevoInstituto = new InstitutoModel();
+                $nuevoInstituto->nombre = $institutoData['nombre'];
+                $nuevoInstituto->codigo = $institutoData['codigo'];
+                
+                $nuevoInstituto->save();
+
+                $this->obj_tipo_respuesta->setok(true);
+                $this->obj_tipo_respuesta->setdata($nuevoInstituto);
+
+            } catch (Exception $e) {
+                $this->obj_tipo_respuesta->setok(false);
+                $this->obj_tipo_respuesta->seterror('Error al crear el instituto', false);
+            }
+            return $this->obj_tipo_respuesta->getdata();
+        }
+
+        public function UpdateInstituto($institutoData)
+        {
+            try {
+                //se busca el insituto a editar utilizando el modelo InstitutoModel
+                $instituto = InstitutoModel::findOrFail($institutoData['id_instituto']);
+
+                $instituto->nombre = $institutoData['nombre'];
+                $instituto->codigo = $institutoData['codigo'];
+
+                $instituto->save();
+
+                $this->obj_tipo_respuesta->setok(true);
+                $this->obj_tipo_respuesta->setdata($instituto);
+            }catch (Exception $e) {
+                $this->obj_tipo_respuesta->setok(false);
+                $this->obj_tipo_respuesta->seterror('Error al editar el instituto', false);
+            }
+
+            return $this->obj_tipo_respuesta->getdata();
+        }
+
+        public function DeleteInstituto($institutoData)
+        {
+            try {
+                //se busca el instituto a eliminar
+                $instituto = InstitutoModel::findOrFail($institutoData);
+
+                //eliminar instituto
+                $instituto->delete();
+
+                $this->obj_tipo_respuesta->setok(true);
+                $this->obj_tipo_respuesta->setdata(null);// no hay datos para devolver después de eliminar
+            }catch (Exception $e) {
+                $this->obj_tipo_respuesta->setok(false);
+                $this->obj_tipo_respuesta->seterror('Error al eliminar el instituto', false);
+            }
+
+            return $this->obj_tipo_respuesta->getdata();
+        }
+
+        public function Consultar($data)
+        {
+            $datos = null;
+            try {
+                switch($data["tipo_consulta"]){
+                    case 1:
+                        //consulta por código
+                        $datos = InstitutoModel::where('codigo', $data["data"])->get();
+                    
+                    case 2:
+                        $datos = InstitutoModel::where('nombre', 'LIKE', '%' . $data["data"] . '%')->get();   
+                }
+                $this->obj_tipo_respuesta->setdata($datos[0]);
+
+            }catch (Exception $e) {
+                $this->obj_tipo_respuesta->setok(false);
+                $this->obj_tipo_respuesta->seterror('Lo sentimos error en la consulta', false);
+            }
+            return $this->obj_tipo_respuesta->getdata();
+        }
+
+
+        
+}
