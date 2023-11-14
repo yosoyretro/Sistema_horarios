230309c50ff685a0dc86a6b4ce8d4943699d2b34
DdyNaTx <moise.jorda107@gmail.com>
2023-11-14 17:33:04 -0500

    asignatura
    estilos

--- resources/views/asignatura.blade.php
+++ resources/views/asignatura.blade.php
@@ -1,3 +1,85 @@
 @extends('aside')
 
 @section('titulo', 'Página de ')
+
+@section('contenido')
+    <div class="container mt-4">
+        <!-- Título de la asignatura -->
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-white bg-primary p-2">Asignatura</h1>
+            </div>
+        </div>
+
+        <!-- Tabla de asignaturas asignadas -->
+        <div class="row mt-4">
+            <div class="col-12">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>Asignatura</th>
+                                <th>Horario</th>
+                                <th>Aula</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Asignaturas asignadas al maestro -->
+                            <tr>
+                                <td>Ética Profesional</td>
+                                <td>Lunes 9:00 - 11:00</td>
+                                <td>Aula 102</td>
+                                <td>
+                                    <button class="btn btn-warning btn-sm" data-toggle="modal" data-target="#editarModal">Editar</button>
+                                    <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#eliminarModal">Eliminar</button>
+                                </td>
+                            </tr>
+                            <!-- Puedes agregar más filas según sea necesario -->
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <!-- CRUD -->
+        <div class="row mt-4">
+            <div class="col-12">
+                <!-- Formulario CRUD aquí -->
+                <form>
+                    <div class="form-row">
+                        <div class="form-group col-md-4">
+                            <label for="asignatura">Asignatura</label>
+                            <input type="text" class="form-control" id="asignatura" placeholder="Nombre de la asignatura">
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="horario">Horario</label>
+                            <input type="text" class="form-control" id="horario" placeholder="Horario de la asignatura">
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="aula">Aula</label>
+                            <input type="text" class="form-control" id="aula" placeholder="Aula de la asignatura">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Agregar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modals de Edición y Eliminación -->
+    <div class="modal" id="editarModal" tabindex="-1" role="dialog" aria-labelledby="editarModalLabel" aria-hidden="true">
+        <!-- ... Contenido del modal de edición ... -->
+    </div>
+
+    <div class="modal" id="eliminarModal" tabindex="-1" role="dialog" aria-labelledby="eliminarModalLabel" aria-hidden="true">
+        <!-- ... Contenido del modal de eliminación ... -->
+    </div>
+
+    <!-- Scripts de Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+@endsection
